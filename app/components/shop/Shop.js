c2d8d152428ae0006acb43e380592777e49401d5
NicholasAzar <nicholas@nicholasazar.com>
2015-12-10 21:55:37 -0500

    Scrapping cards.. will rewrite in css.

--- app/components/shop/Shop.js
+++ app/components/shop/Shop.js
@@ -1,14 +1,6 @@
 var React = require('react');
 var history = require('../common/history');
 
-const Card = require('material-ui/lib/card/card');
-const CardActions = require('material-ui/lib/card/card-actions');
-const CardExpandable = require('material-ui/lib/card/card-expandable');
-const CardHeader = require('material-ui/lib/card/card-header');
-const CardMedia = require('material-ui/lib/card/card-media');
-const CardText = require('material-ui/lib/card/card-text');
-const CardTitle = require('material-ui/lib/card/card-title');
-
 const RaisedButton = require('material-ui/lib/raised-button');
 
 var Shop = React.createClass({
@@ -17,38 +9,27 @@ var Shop = React.createClass({
 			products: [{test: "test"}]
 		});
 	},
-
-	getStyles: function() {
-		return({
-			product: {
-				width: '300px',
-				height: '400px',
-				left: '50%',
-				marginLeft: '-150px',
-				marginTop: '20px'
-			}
-		});
-	},
 	render: function() {
-		let styles = this.getStyles();
 		return (
-			<div className="shop">
+			<div>
 				<div className="header">
 					<h2 className="headerContent">Shop</h2>
 				</div>
-				<div>
+				<div className="shop">
 				{
 					this.state.products.map(function(product){
 						return (
-							<Card style={styles.product}>
-								<CardMedia overlay={<CardTitle title="Title" subtitle="Subtitle"/>}>
+							<div className="product">
+								<div className="productDisplay">
 									<img src="http://lorempixel.com/300/350/nature/"/>
-								</CardMedia>
-								<CardActions>
+									<span className="productTitle">Title</span>
+									<span className="productSubtitle">Subtitle</span>
+								</div>
+								<div className="productButtons">
 									<RaisedButton label="Action1"/>
 									<RaisedButton label="Action2"/>
-								</CardActions>
-							</Card>
+								</div>
+							</div>
 						);
 					}, this)
 				}
