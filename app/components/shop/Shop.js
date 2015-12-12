eab8ae0fb1cd18c43e2fb784545d229c90a46730
NicholasAzar <nicholas@nicholasazar.com>
2015-12-12 13:17:22 -0500

    Modal display image on view full

--- app/components/shop/Shop.js
+++ app/components/shop/Shop.js
@@ -2,13 +2,58 @@ var React = require('react');
 var history = require('../common/history');
 
 const RaisedButton = require('material-ui/lib/raised-button');
+const Dialog = require('material-ui/lib/dialog');
 
 var Shop = React.createClass({
 	getInitialState: function() {
 		return({
-			products: [{test: "test"}, {test: "test"}, {test: "test"}, {test: "test"}, {test: "test"}]
+			products: [
+				{
+					title: 'Title1',
+					subTitle: 'Subtitle',
+					imageLink: 'http://lorempixel.com/300/350/nature/'
+				},
+				{
+					title: 'Title2',
+					subTitle: 'Subtitle',
+					imageLink: 'http://lorempixel.com/300/350/nature/'
+				},
+				{
+					title: 'Title3',
+					subTitle: 'Subtitle',
+					imageLink: 'http://lorempixel.com/300/350/nature/'
+				},
+				{
+					title: 'Title4',
+					subTitle: 'Subtitle',
+					imageLink: 'http://lorempixel.com/300/350/nature/'
+				},
+				{
+					title: 'Title5',
+					subTitle: 'Subtitle',
+					imageLink: 'http://lorempixel.com/300/350/nature/'
+				}
+			],
+			dialogIsDisplayed: false,
+			dialogTitle: '',
+			dialogImage: ''
 		});
 	},
+
+	onViewFull: function(product) {
+		console.log("View Full:", product);
+		this.setState({
+			dialogIsDisplayed: true,
+			dialogTitle: product.title,
+			dialogImage: product.imageLink
+		})
+	},
+	onViewClose: function() {
+		this.setState({
+			dialogIsDisplayed: false,
+		});
+	},
+
 	render: function() {
 		return (
 			<div>
@@ -22,16 +67,16 @@ var Shop = React.createClass({
 							<div className="productWrapper">
 								<div className="product">
 									<div className="productDisplay">
-										<img src="http://lorempixel.com/300/350/nature/"/>
+										<img src={product.imageLink}/>
 										<span className="productOverlay">
 											<span className="productTextOverlay">
-												<span className="productTitle">Title</span>
-												<span className="productSubtitle">Subtitle</span>
+												<span className="productTitle">{product.title}</span>
+												<span className="productSubtitle">{product.subTitle}</span>
 											</span>
 										</span>
 									</div>
 									<div className="productButtons">
-										<RaisedButton label="View Full" secondary={true} style={{margin: '5px'}}/>
+										<RaisedButton label="View Full" onTouchTap={this.onViewFull.bind(this, product)} secondary={true} style={{margin: '5px'}}/>
 										<RaisedButton label="Buy" primary={true} style={{margin: '5px'}}/>
 									</div>
 								</div>
@@ -40,6 +85,12 @@ var Shop = React.createClass({
 					}, this)
 				}
 				</div>
+				<Dialog
+					title={this.state.dialogTitle}
+					open={this.state.dialogIsDisplayed}
+					onRequestClose={this.onViewClose}>
+					<img src={this.state.dialogImage} />
+				</Dialog>
 			</div>
 		);
 	}
